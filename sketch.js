0348446f760f48396efa2d399aa537935c989d10
murphysoup <murphysoupsicus@gmail.com>
2025-08-03 08:29:02 +1000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,49 +15,49 @@ import MovingSquare from './MovingSquare.js';
 
 const sketch = (p) => {
 
-function preload() {
-  texturesmap = loadImage('assets/maps/map1.jpg');
+preload = () => {
+  texturesmap = p.loadImage('assets/maps/map1.jpg');
 }
 
-function setup() {
-  createCanvas(800, 600);
+setup = () => {
+  p.createCanvas(800, 600);
   player = {
-    x: width / 2,
-    y: height / 2,
+    x: p.width / 2,
+    y: p.height / 2,
     sqsize: 32,
   };
 
   movingSquares.push(new MovingSquare(-2000, -2000, 2000, texturesmap));
 
   for (let i = 0; i < numSquares; i++) {
-    let x = random(-2000, 2000);
-    let y = random(-2000, 2000);
+    let x = p.random(-2000, 2000);
+    let y = p.random(-2000, 2000);
     let sqsize = 20;
     let tex = texturesmap;
     movingSquares.push(new MovingSquare(x, y, sqsize, tex));
   }
 }
 
-function draw() {
-  background(240);
+p.draw = () => {
+  p.background(240);
 
   let dx = 0;
   let dy = 0;
-  if (keyIsDown(87)) dy += speed;
-  if (keyIsDown(83)) dy -= speed;
-  if (keyIsDown(65)) dx += speed;
-  if (keyIsDown(68)) dx -= speed;
+  if (p.keyIsDown(87)) dy += speed;
+  if (p.keyIsDown(83)) dy -= speed;
+  if (p.keyIsDown(65)) dx += speed;
+  if (p.keyIsDown(68)) dx -= speed;
 
   tickUpdate(movingSquares, dx, dy);
 
   for (let sq of movingSquares) {
-    sq.display();
+    sq.display(p);
   }
 
-  fill(255, 50, 50);
-  noStroke();
-  rectMode(CENTER);
-  rect(player.x, player.y, player.sqsize, player.sqsize);
+  p.fill(255, 50, 50);
+  p.noStroke();
+  p.rectMode(CENTER);
+  p.rect(player.x, player.y, player.sqsize, player.sqsize);
 }
 
 }
