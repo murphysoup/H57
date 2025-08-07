3b1e6e1bc85f805348358f77b595ca020a1003bf
murphysoup <murphysoupsicus@gmail.com>
2025-08-07 16:35:45 +1000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -87,7 +87,7 @@ p.draw = () => {
   if (p.keyIsDown(83)) dy -= speed;
   if (p.keyIsDown(65)) dx += speed;
   if (p.keyIsDown(68)) dx -= speed;
-  if (1=2)
+  //if (1=2)
   p.globx -= dx;
   p.globy -= dy;
   tickUpdate(movingSquares, dx, dy, p);
