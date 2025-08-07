159c91caabbad588bdd7497791cc13ab30aecc9f
murphysoup <murphysoupsicus@gmail.com>
2025-08-07 16:38:57 +1000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -27,7 +27,7 @@ p.preload = () => {
 };
 
 p.addbox = (g,x1,y1,x2,y2) => {
- g[floor(x1/128)][floor(y1/128)].append([x1,y1,x2,y2])
+ g[Math.floor(x1/128)][Math.floor(y1/128)].append([x1,y1,x2,y2])
  
 }
   
