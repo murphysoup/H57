1c58d4688a752a6ee5f5ff25f5f16db1ee7d2507
murphysoup <murphysoupsicus@gmail.com>
2025-08-25 16:59:26 +1000

    Update MovingSquare.js

--- MovingSquare.js
+++ MovingSquare.js
@@ -32,7 +32,7 @@ delete(p) {
     this.y += dy;
     this.objectsprite.position.x = this.x;
     this.objectsprite.position.y = this.y;
-    if (this.type = 'food' && p.dist(this.x,this.y,p.globx,p.globy)){
+    if (this.type = 'food' && p.dist(this.x,this.y,p.globx,p.globy) < 10){
     p.hunger += 1
     this.delete(p)
     }
