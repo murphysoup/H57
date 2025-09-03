21aa85ece37d61692b5e8d858b12a9d812b1d8bf
murphysoup <murphysoupsicus@gmail.com>
2025-09-03 16:33:37 +1000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -120,7 +120,7 @@ p.draw = () => {
   p.hunger = p.hunger.toFixed(5)
 
 
- if (p.hunger > 0 || p.hp > 0)
+ if (p.hunger > 0 || p.hp > 0) {
   if (p.hunger < 0) {p.hunger = 0.1; p.hp -= 0.1 }
   if (p.hunger < 0) {p.hp = 0.1; p.hunger -= 0.1 }
 
